b8fe630c69db691bba5008e9a654bb8b052253e0
ByronRoberts <byron.p.roberts@gmail.com>
2018-12-21 12:43:57 -0600

    Survey/Data/Home

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,9 @@
+var friendsData = [
+    {
+      name: "Byron 'Byronious Rex' Roberts",
+      photo: "https://tribzap2it.files.wordpress.com/2016/04/baby-sinclair-dinosaurs-abc.jpg?w=900",
+      scores: [4, 5, 5, 1, 4, 2, 4, 3, 5, 5]
+    },
+]
+
+module.exports = friendsData
