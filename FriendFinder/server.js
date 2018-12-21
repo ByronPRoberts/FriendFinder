1acca4a5768e92e4f567dfa8729910c8b0db816f
ByronRoberts <byron.p.roberts@gmail.com>
2018-12-21 14:05:55 -0600

    express var fix

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,10 +1,10 @@
-var express = requrire('express');
+var express = require('express');
 var path = require('path');
 var apiRoutes = require('./app/routing/apiRoutes.js');
 var htmlRoutes = require('./app/routing/htmlRoutes.js');
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
